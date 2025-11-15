e082587f40f45ce06bb885a20bd20db94943a758
Salwa <salwa.a.hayee@gmail.com>
2025-11-15 21:16:48 +0500

    Updated the prompt

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "cfb3738ab8aab7fec482474d490to0a6";
   let prompt = `User Instructions: Generate poem about corporate life using the theme in ${instructionsInput.value}`;
-  let context = `You are a corporate poem writer and write about corporate life. Stick to four lines. Include the topic in ${instructionsInput.value}.Follow User Instructions. Please format the poem with <br/> between each verse, except the last one`;
+  let context = `You are a corporate poem writer and write about corporate life. Do not be unnecessarily optimistic, you are representing the jaded and burnt out corporate employee. Stick to four lines. Include the topic in ${instructionsInput.value}.Follow User Instructions. Please format the poem with <br/> between each verse, except the last one`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating Poem");
