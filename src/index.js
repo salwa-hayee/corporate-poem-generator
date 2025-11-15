e813197bd84ba7a6b7c98affbb8d249671e7c54c
Salwa <salwa.a.hayee@gmail.com>
2025-11-15 22:40:56 +0500

    finished the project with animations in CSS, javascript  and HTML to improve the user experience

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ function generatePoem(event) {
   let context = `You are a corporate poem writer and write about corporate life. Do not be unnecessarily optimistic, you are representing the jaded and burnt out corporate employee. Stick to four lines. Include the topic in ${instructionsInput.value}.Follow User Instructions. Please format the poem with <br/> between each verse, except the last one`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating Poem");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating blink">📝Writing your poem about corporate grind under the theme: ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
